380994b992e581412617c5042b691dc307464b28
puunderlinepuru <damnthatslitterallyme@gmail.com>
2026-02-24 14:34:33 +0300

    trying to do auth

---
+++ Data handler/data-handler.js
@@ -0,0 +1,46 @@
+const path = require('node:path');
+const process = require('node:process');
+const {authenticate} = require('@google-cloud/local-auth');
+const {google} = require('googleapis');
+
+// The scope for reading spreadsheets.
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+// The path to the credentials file.
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+
+/**
+ * Prints the names and majors of students in a sample spreadsheet:
+ * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ */
+async function listMajors() {
+  // Authenticate with Google and get an authorized client.
+console.log("check")
+
+  const auth = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+
+  console.log("authentication check")
+
+  // Create a new Sheets API client.
+  const sheets = google.sheets({version: 'v4', auth});
+  // Get the values from the spreadsheet.
+  const result = await sheets.spreadsheets.values.get({
+    spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
+    range: 'Class Data!A2:E',
+  });
+  const rows = result.data.values;
+  if (!rows || rows.length === 0) {
+    console.log('No data found.');
+    return;
+  }
+  console.log('Name, Major:');
+  // Print the name and major of each student.
+  rows.forEach((row) => {
+    // Print columns A and E, which correspond to indices 0 and 4.
+    console.log(`${row[0]}, ${row[4]}`);
+  });
+}
+
+listMajors();
